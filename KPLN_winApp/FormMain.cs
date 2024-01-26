8f2a5d1462b5836a9055ed17770d3c974239a053
xPRYANIKx <tuleninov.rv@gmail.com>
2024-01-26 10:51:10 +0300

    add CheckWebsiteAvailability + general refactoring

--- KPLN_winApp/FormMain.cs
+++ KPLN_winApp/FormMain.cs
@@ -9,27 +9,59 @@ using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
 
 
 namespace KPLN_winApp
 {
     public partial class FormMain : System.Windows.Forms.Form
     {
-
+        // Инициализация компонентов Chromium 
+        private void chromiumWebBrowser_LoadingStateChanged(object sender, CefSharp.LoadingStateChangedEventArgs e)
+        {
+        }
+        // Функция проверки доступа на сайт
+        public static bool CheckWebsiteAvailability(string url)
+        {
+            try
+            {
+                using (var client = new WebClient())
+                {
+                    string response = client.DownloadString(url);
+                    if (response.Contains("Строчка вшитая в HTML, еслии страница не может прогрузиться"))
+                    {
+                        return false;
+                    }
+                    return true;
+                }
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+        // 
+        //
+        //
+        //
         public FormMain()
         {
             InitializeComponent();
         }
-
         private void FormMain_Load(object sender, EventArgs e)
         {
-            // Загрузка содержимого файла "users.ini"
-            string fileContent = File.ReadAllText("users.ini");
-
+            // Проверка доступа на сайт и загрузка страницы
+            string url = "https://kpln-employees.ru/";
+            bool webResponse = CheckWebsiteAvailability(url);
+            if (!webResponse)
+            {
+                Application.Exit();
+            } else { chromiumWebBrowser.LoadUrl(url); }
             // Получение имени пользователя, авторизованного в системе
             string currentUser = Environment.UserName;
-
-            // Проверка строк файла "users.ini"
+            // Загрузка содержимого файла "users.ini" и проверка егом строк
+            string fileContent = File.ReadAllText("users.ini");
             string[] lines = fileContent.Split(new[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
             foreach (string line in lines)
             {
@@ -39,24 +71,15 @@ namespace KPLN_winApp
                     string username = parts[0].Trim();
                     string authorized = parts[1].Trim();
 
-                    // Принудительное закрытие программы
-                    if ((username == currentUser && authorized == "1")) 
+                    if (username == currentUser && authorized == "1")
                     {
                         Application.Exit();
                     }
                 }
             }
-            // Загрузка страницы
-            chromiumWebBrowser.LoadUrl("https://kpln-employees.ru/");
-
             // Отладка
-            Console.WriteLine(currentUser);
+            Console.WriteLine("Пользователь Windows: " + currentUser);
+            Console.WriteLine("Доступ к внешнему ресурсу: " + webResponse);
         }
-
-        private void chromiumWebBrowser_LoadingStateChanged(object sender, CefSharp.LoadingStateChangedEventArgs e)
-        {
-
-        }
-
     }
 }
